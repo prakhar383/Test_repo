544038f6c5250de6fe8b03bc97cde84b0a49e8ce
Prakhar Chandrakar <prakhar3823@gmail.com>
2026-06-01 13:41:50 +0530

    merge conflict test

--- divide.py
+++ divide.py
@@ -1,5 +1,2 @@
 def divide(a, b):
-    """Returns the quotient of a and b. Raises ValueError on division by zero."""
-    if b == 0:
-        raise ValueError("Cannot divide by zero.")
-    return a / b
+    "edited to see merge confilict"
